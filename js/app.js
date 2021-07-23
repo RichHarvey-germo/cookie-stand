fa060b32a89c9a1b25959dc9b41eb4f73a3c75fa
Richard Harvey <germoisawesome@gmail.com>
2021-07-22 20:50:50 -0700

    Made css/html changes. Added form

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
-'use strict';
+"use strict";
 
-console.log('yes');
-
+console.log("yes");
 
 // -------------------------------------------- Global Variables ------------------------------------------------------ //
 
-const tableElem = document.getElementById('sales');
+const tableElem = document.getElementById("sales");
 
-
-
-const hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const hoursOfOperation = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
 const storeArr = [i];
-
 
 // -------------------------------------------- Constructor Function --------------------------------------------------- //
 
@@ -30,39 +41,39 @@ function Store(name, minCust, maxCust, averageCookiesPerSale) {
 
 Store.allStores = [];
 
-
-
 Store.prototype.randomCustInRange = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  return Math.floor(
+    Math.random() * (this.maxCust - this.minCust + 1) + this.minCust
+  );
 };
-
 
 Store.prototype.calculateSalesPerHour = function () {
   for (let i = 0; i < hoursOfOperation.length; i++) {
-    const thisHoursSale = Math.ceil(this.randomCustInRange() * this.averageCookiesPerSale);
+    const thisHoursSale = Math.ceil(
+      this.randomCustInRange() * this.averageCookiesPerSale
+    );
     this.salesHourly.push(thisHoursSale);
   }
 };
 
-
 Store.prototype.renderStore = function (bodyElem) {
   let grandTotal = 0;
 
-  const rowElem = document.createElement('tr');
+  const rowElem = document.createElement("tr");
   bodyElem.appendChild(rowElem);
 
-  const locationTHElem = document.createElement('th');
+  const locationTHElem = document.createElement("th");
   locationTHElem.textContent = this.name;
   rowElem.appendChild(locationTHElem);
 
   for (let i = 0; i < this.salesHourly.length; i++) {
     const hourlyTotal = this.salesHourly[i];
-    const tdElem = document.createElement('td');
+    const tdElem = document.createElement("td");
     tdElem.textContent = hourlyTotal;
     grandTotal += hourlyTotal;
     rowElem.appendChild(tdElem);
   }
-  const grandTotalTHElem = document.createElement('th');
+  const grandTotalTHElem = document.createElement("th");
   grandTotalTHElem.textContent = grandTotal;
   rowElem.appendChild(grandTotalTHElem);
 };
@@ -78,20 +89,18 @@ function makeElement(tagName, parent, textContent) {
   return element;
 }
 
-
 function renderHeader() {
-  const headerElem = makeElement('thead', tableElem, null);
-  const rowElem = makeElement('tr', headerElem, null);
-  makeElement('th', rowElem, 'city');
+  const headerElem = makeElement("thead", tableElem, null);
+  const rowElem = makeElement("tr", headerElem, null);
+  makeElement("th", rowElem, "city");
   for (let i = 0; i < hoursOfOperation.length; i++) {
-    makeElement('th', rowElem, hoursOfOperation[i]);
+    makeElement("th", rowElem, hoursOfOperation[i]);
   }
-  makeElement('th', rowElem, 'Daily Total Sales');
+  makeElement("th", rowElem, "Daily Total Sales");
 }
 
-
 function renderAllStores() {
-  const bodyElem = makeElement('tbody', tableElem, null);
+  const bodyElem = makeElement("tbody", tableElem, null);
   for (let i = 0; i < Store.allStores.length; i++) {
     let currentStore = Store.allStores[i];
     currentStore.calculateSalesPerHour();
@@ -99,15 +108,13 @@ function renderAllStores() {
   }
 }
 
-
-
 // -------------------------------------------- Call Functions --------------------------------------------------------- //
 
-const seattle = new Store('Seattle', 23, 65, 6.3);
-const tokyo = new Store('Tokyo', 3, 24, 1.2);
-const dubai = new Store('Dubai', 11, 38, 3.7);
-const paris = new Store('Paris', 20, 38, 2.3);
-const lima = new Store('Lima', 2, 16, 4.6);
+const seattle = new Store("Seattle", 23, 65, 6.3);
+const tokyo = new Store("Tokyo", 3, 24, 1.2);
+const dubai = new Store("Dubai", 11, 38, 3.7);
+const paris = new Store("Paris", 20, 38, 2.3);
+const lima = new Store("Lima", 2, 16, 4.6);
 
 // seattle.calculateSalesPerHour();
 // seattle.randomCustInRange();
